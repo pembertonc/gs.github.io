afee8ed702709019cfdc910789903b477412cfc0
Clive Stewart <clivestewart@hotmail.com>
2021-12-14 09:42:31 -0400

    Added Authenticated and UnAuthenticated pages

---
+++ src/main/java/com/techmine/gs/ui/pages/authenticated_base_page/AuthenticatedBasePage.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2021 Work.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.techmine.gs.ui.pages.authenticated_base_page;
+
+import com.techmine.gs.ui.pages.unauthenticated_base_page.UnAuthenticatedBasePage;
+
+/**
+ *
+ * @author Work
+ */
+public class AuthenticatedBasePage extends UnAuthenticatedBasePage {
+
+    public AuthenticatedBasePage() {
+    }
+
+}
