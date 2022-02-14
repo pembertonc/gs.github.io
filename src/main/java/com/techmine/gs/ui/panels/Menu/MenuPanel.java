3b42d4f20ef588c08062429982a5e0392247f730
pembertonc <cedric.pemberton@gmail.com>
2022-02-13 20:20:44 -0400

    Added Menu Structure to MenuPanel

---
+++ src/main/java/com/techmine/gs/ui/panels/Menu/MenuPanel.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 Cedric-Pemberton.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.techmine.gs.ui.panels.Menu;
+
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+
+/**
+ *
+ * @author Cedric-Pemberton
+ */
+public class MenuPanel extends Panel {
+
+    public MenuPanel(String id) {
+        super(id);
+    }
+
+    public MenuPanel(String id, IModel<?> model) {
+        super(id, model);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+}
