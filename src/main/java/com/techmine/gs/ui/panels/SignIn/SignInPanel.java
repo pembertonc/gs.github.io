f85382ef6e97411cd7b746493f979a5035dac46f
pembertonc <cedric.pemberton@gmail.com>
2022-02-14 10:38:39 -0400

    Created components for signInForm userName and Password Html template fields

--- src/main/java/com/techmine/gs/ui/panels/SignIn/SignInPanel.java
+++ src/main/java/com/techmine/gs/ui/panels/SignIn/SignInPanel.java
@@ -15,14 +15,22 @@
  */
 package com.techmine.gs.ui.panels.SignIn;
 
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LambdaModel;
+import org.apache.wicket.protocol.https.RequireHttps;
 
 /**
  *
  * @author Cedric-Pemberton
  */
 public class SignInPanel extends Panel {
+
+    Form<Void> signInForm;
+    private Subject user;
 
     public SignInPanel(String id) {
         super(id);
@@ -32,4 +40,37 @@ public class SignInPanel extends Panel {
         super(id, model);
     }
 
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        user = new Subject();
+        this.signInForm = new Form("signInForm");
+        add(signInForm);
+        signInForm.add(new TextField<>("userName", LambdaModel.of(user::getUserName, user::setUserName)));
+        signInForm.add(new PasswordTextField("password", LambdaModel.of(user::getPassword, user::setPassword)));
+
+    }
+
+    private class Subject {
+
+        private String userName;
+        private String Password;
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+        public String getPassword() {
+            return Password;
+        }
+
+        public void setPassword(String Password) {
+            this.Password = Password;
+        }
+
+    }
 }
