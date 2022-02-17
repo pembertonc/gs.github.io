dbd94f6b2c2af36a50fa97dc713e61a40e300267
pembertonc <cedric.pemberton@gmail.com>
2022-02-17 18:45:53 -0400

    Added to manage persistence for Subject

---
+++ src/main/java/com/techmine/gs/repository/SubjectRepository.java
@@ -0,0 +1,21 @@
+package com.techmine.gs.repository;
+
+import javax.persistence.EntityManager;
+import javax.inject.Inject;
+import com.techmine.gs.domain.Subject;
+
+public class SubjectRepository extends AbstractRepository<Subject, String> {
+
+    @Inject
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public SubjectRepository() {
+        super(Subject.class);
+    }
+
+}
