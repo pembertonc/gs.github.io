11d7c599fc7a863dc6f06f57773e5e21e8a8fb51
pembertonc <cedric.pemberton@gmail.com>
2022-02-23 12:43:23 -0400

    Updated class to contain a collection of AuthenticationRoles in stead of a single role

--- src/main/java/com/techmine/gs/domain/Subject.java
+++ src/main/java/com/techmine/gs/domain/Subject.java
@@ -11,6 +11,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 /**
@@ -23,16 +24,17 @@ public class Subject extends BaseEntity {
     @Basic(optional = false)
     @Column(unique = true, nullable = false, length = 64)
     @NotBlank(message = "User Name is required")
-    private String userName;
+    private String userName = "";
     @Basic(optional = false)
     @Column(nullable = false, length = 12)
     @NotBlank(message = "Password is required")
     @Size(min = 4, max = 12, message = "Password must be from 4 to 12 characters long.")
-    private String password;
-    @OneToOne
-    private Person person;
+    private String password = "";
+    @OneToOne(optional = false)
+    @NotNull
+    private Person person = new Person();
     @ManyToMany
-    private List<Role> roles;
+    private List<AuthorizationRole> authorizationRoles = new ArrayList<AuthorizationRole>();
 
     public String getUserName() {
         return userName;
@@ -73,28 +75,28 @@ public class Subject extends BaseEntity {
         return this;
     }
 
-    public List<Role> getRoles() {
-        if (roles == null) {
-            roles = new ArrayList<>();
+    public List<AuthorizationRole> getAuthorizationRoles() {
+        if (authorizationRoles == null) {
+            authorizationRoles = new ArrayList<>();
         }
-        return roles;
+        return authorizationRoles;
     }
 
-    public void setRoles(List<Role> roles) {
-        this.roles = roles;
+    public void setAuthorizationRoles(List<AuthorizationRole> authorizationRoles) {
+        this.authorizationRoles = authorizationRoles;
     }
 
-    public Subject roles(List<Role> roles) {
-        this.roles = roles;
+    public Subject authorizationRoles(List<AuthorizationRole> authorizationRoles) {
+        this.authorizationRoles = authorizationRoles;
         return this;
     }
 
-    public void addRole(Role role) {
-        getRoles().add(role);
+    public void addAuthorizationRole(AuthorizationRole authorizationRole) {
+        getAuthorizationRoles().add(authorizationRole);
     }
 
-    public void removeRole(Role role) {
-        getRoles().remove(role);
+    public void removeAuthorizationRole(AuthorizationRole authorizationRole) {
+        getAuthorizationRoles().remove(authorizationRole);
     }
 
     @Override
@@ -124,6 +126,10 @@ public class Subject extends BaseEntity {
     @Override
     public String toString() {
         return "Subject{" + " userName=" + userName + ", password=" + password + '}';
+    }
+
+    public Subject getObject() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
 }
