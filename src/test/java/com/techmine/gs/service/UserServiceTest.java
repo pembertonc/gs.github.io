9b67ddfcb5b42c530e91b4fac9ce7d8ed746aac0
pembertonc <cedric.pemberton@gmail.com>
2022-02-28 14:17:40 -0400

    Updated CreateSubject method associated with class change related to removing list of Contacts in Person

--- src/test/java/com/techmine/gs/service/UserServiceTest.java
+++ src/test/java/com/techmine/gs/service/UserServiceTest.java
@@ -13,15 +13,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// Rewrite this test to use arquillian
 package com.techmine.gs.service;
 
+import com.techmine.gs.domain.Contact;
+import com.techmine.gs.domain.Person;
+import com.techmine.gs.domain.Subject;
+import com.techmine.gs.repository.AbstractRepository;
 import com.techmine.gs.repository.SubjectRepository;
-import com.techmine.gs.testutils.TestEntityManagerProvider;
-
+import com.techmine.gs.testutils.TestEntityManagerProducer;
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
-import javax.enterprise.inject.Alternative;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
+
 import javax.persistence.EntityManager;
+
+import javax.transaction.Transactional;
 
 import org.jboss.weld.junit5.auto.ActivateScopes;
 import org.jboss.weld.junit5.auto.AddBeanClasses;
@@ -33,6 +41,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 /**
@@ -40,16 +49,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
  * @author Cedric-Pemberton
  */
 @ExtendWith(WeldJunit5AutoExtension.class)
-@AddBeanClasses(value = {TestEntityManagerProvider.class, UserServiceTest.class, UserService.class, SubjectRepository.class})
-@EnableAlternatives(value = {TestEntityManagerProvider.class})
-@ActivateScopes(value = {RequestScoped.class})
+@AddBeanClasses(value = {TestEntityManagerProducer.class, UserServiceTest.class, UserService.class, SubjectRepository.class, AbstractRepository.class})
+@EnableAlternatives(value = {TestEntityManagerProducer.class})
+@ActivateScopes(value = {RequestScoped.class, SessionScoped.class, ApplicationScoped.class})
 public class UserServiceTest {
 
     @Inject
     UserService instance;
 
     @Inject
-    @Alternative
     EntityManager em;
 
     public UserServiceTest() {
@@ -76,7 +84,6 @@ public class UserServiceTest {
      */
     @Test
     public void testUsersServiceInjectable() {
-        System.out.println("createUser");
 
         assertNotNull(this.instance);
     }
@@ -85,6 +92,45 @@ public class UserServiceTest {
     public void testInjected() {
         assertNotNull(this.em);
 
+    }
+
+    @Test
+    @Transactional
+    public void testcreateSubject() {
+        System.out.println("createUser");
+        Subject sub = createTestSubject();
+        //this.em.getTransaction().begin();
+        //assertTrue(this.instance.checkUserExistxByUserName("Administrator"));
+        //instance.createUser(sub);
+        this.em.getTransaction().begin();
+        this.em.persist(sub);
+        this.em.getTransaction().commit();
+        //this.em.flush();
+        // this.em.getTransaction().commit();
+    }
+
+    @Test
+    @Disabled
+    public void testPersistenceContextInjection() {
+        //  assertNotNull(entityManager);
+    }
+
+    private Subject createTestSubject() {
+
+        Contact contact = new Contact()
+                .email("user1@gmail.com")
+                .telephone1("767-245-2656")
+                .telephone2("767-245-5455");
+        Person person = new Person()
+                .familyName("John")
+                .firstName("Peter")
+                .otherName("Derick")
+                .contact(contact);
+
+        Subject subject = new Subject();
+        return subject.password("Password")
+                .userName("User1")
+                .person(person);
     }
 
 }
