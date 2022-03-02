83c7a0119192b9c2e428efadb212fffb132989c4
pembertonc <cedric.pemberton@gmail.com>
2022-03-02 09:24:14 -0400

    AbstractInputFieldWFeedbackAndCaption will allow the a sub class to replace the Input html tag so that Different Field types -- TextField, PasswordTextField, NumberField ... _ can be instantiated and used internally. The each require types or text, password, numer, email etc.

---
+++ src/main/java/com/techmine/gs/ui/panels/custom_input_components/TextFieldWithMessage/AbstractInputFieldWFeedbackAndCaption.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2022 Cedric-Pemberton.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.techmine.gs.ui.panels.custom_input_components.TextFieldWithMessage;
+
+import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.markup.IMarkupCacheKeyProvider;
+import org.apache.wicket.markup.IMarkupResourceStreamProvider;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.util.resource.IResourceStream;
+
+/**
+ *
+ * @author Cedric-Pemberton
+ */
+public abstract class AbstractInputFieldWFeedbackAndCaption extends Panel implements IMarkupResourceStreamProvider, IMarkupCacheKeyProvider {
+
+    public AbstractInputFieldWFeedbackAndCaption(String id, IModel<?> model) {
+        super(id, model);
+    }
+
+    /*
+    @Override
+    public IResourceStream getMarkupResourceStream(MarkupContainer mc, Class<?> type) {
+    throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+     */
+    @Override
+    public String getCacheKey(MarkupContainer mc, Class<?> type) {
+        return null;
+    }
+
+}
