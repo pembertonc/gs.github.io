0e95b9258639af1440eaa6d3b0bac9e9cc0de78b
pembertonc <cedric.pemberton@gmail.com>
2022-03-02 12:12:47 -0400

    Added Methods to determing and create Specific TextField decendent base on FieldType specified in constructor.

--- src/main/java/com/techmine/gs/ui/panels/custom_input_components/TextFieldWithMessage/AbstractInputFieldWFeedbackAndCaption.java
+++ src/main/java/com/techmine/gs/ui/panels/custom_input_components/TextFieldWithMessage/AbstractInputFieldWFeedbackAndCaption.java
@@ -15,36 +15,133 @@
  */
 package com.techmine.gs.ui.panels.custom_input_components.TextFieldWithMessage;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
+import org.apache.wicket.feedback.ComponentFeedbackMessageFilter;
 import org.apache.wicket.markup.IMarkupCacheKeyProvider;
 import org.apache.wicket.markup.IMarkupResourceStreamProvider;
-import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.EmailTextField;
+import org.apache.wicket.markup.html.form.FormComponent;
+import org.apache.wicket.markup.html.form.FormComponentPanel;
+import org.apache.wicket.markup.html.form.NumberTextField;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.util.resource.IResourceStream;
+import org.apache.wicket.validation.IValidator;
 
 /**
+ * Creates a component that contains a internal TextFieldDecedent. The type of
+ * Decendent is indicated by passing A FieldType to the Constructor. The
+ * Initialized TexField is passed an an AjaxtFormComponentUpdatingBehavior.
+ * Validation is done by calling the
  *
  * @author Cedric-Pemberton
  */
-public abstract class AbstractInputFieldWFeedbackAndCaption extends Panel implements IMarkupResourceStreamProvider, IMarkupCacheKeyProvider {
+public abstract class AbstractInputFieldWFeedbackAndCaption extends FormComponentPanel implements IMarkupResourceStreamProvider, IMarkupCacheKeyProvider {
+
+    protected FormComponent formComponent;
+    protected Label caption;
+    protected FeedbackPanel errorMessage;
+    protected FieldType fieldType;
+    protected List<IValidator> validators;
+    protected String captionValue;
 
     private AbstractInputFieldWFeedbackAndCaption(String id, IModel<?> model) {
         super(id, model);
     }
 
-    public AbstractInputFieldWFeedbackAndCaption(String id, IModel<?> model, FieldType fieldType) {
-        super(id, model);
+    public AbstractInputFieldWFeedbackAndCaption(String id, IModel<?> model, String captionValue, FieldType fieldType) {
+        this(id, model);
+        this.fieldType = fieldType;
+        this.validators = new ArrayList();
+        this.captionValue = captionValue;
+
     }
 
-    /*
     @Override
-    public IResourceStream getMarkupResourceStream(MarkupContainer mc, Class<?> type) {
-    throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    protected void onInitialize() {
+        super.onInitialize();
+        add(caption = new Label("fieldCaption", captionValue));
+        add(formComponent = initializeInputField(fieldType, getDefaultModel(), captionValue));
+        addAjaxUpdatingBehaviorToFormComponent();
+        applyValidators();
+        add(errorMessage = new FeedbackPanel("errorMessage", new ComponentFeedbackMessageFilter(formComponent)));
     }
-     */
+
     @Override
     public String getCacheKey(MarkupContainer mc, Class<?> type) {
         return null;
+    }
+
+    private void addAjaxUpdatingBehaviorToFormComponent() {
+        formComponent.add(new AjaxFormComponentUpdatingBehavior("blur") {
+            @Override
+            protected void onUpdate(AjaxRequestTarget target) {
+                Optional.of(target).ifPresent((AjaxRequestTarget t) -> t.add(errorMessage));
+            }
+
+            @Override
+            protected void onError(AjaxRequestTarget target, RuntimeException e) {
+                super.onError(target, e);
+                Optional.of(target).ifPresent((AjaxRequestTarget t) -> t.add(errorMessage));
+            }
+        });
+    }
+
+    private void applyValidators() {
+        validators.iterator().forEachRemaining((IValidator v) -> formComponent.add(v));
+
+    }
+
+    protected FormComponentPanel addInputFieldValidator(IValidator validator) {
+        this.validators.add(validator);
+        return this;
+
+    }
+
+    protected FormComponent initializeInputField(FieldType fieldType, IModel model, String id) {
+        switch (fieldType) {
+            case TEXT:
+                return initializeTextField(model, id);
+
+            case NUMBER:
+                return initializeNumberTextField(model, id);
+
+            case PASSWORD:
+                return initializePasswordTextField(model, id);
+
+            case EMAIL:
+                return initializeEmailTextField(model, id);
+
+            default:
+                throw new UnsupportedOperationException("InputFieldWFeedbackAndCaption does not support the specified field type");
+        }
+
+    }
+
+    private FormComponent initializeTextField(IModel model, String id) {
+        return new TextField(id, model);
+    }
+
+    private FormComponent initializeNumberTextField(IModel model, String id) {
+        return new NumberTextField(id, model);
+    }
+
+    private FormComponent initializePasswordTextField(IModel model, String id) {
+        return new PasswordTextField(id, model);
+
+    }
+
+    private FormComponent initializeEmailTextField(IModel model, String id) {
+
+        return new EmailTextField(id, model);
+
     }
 
     public static enum FieldType {
