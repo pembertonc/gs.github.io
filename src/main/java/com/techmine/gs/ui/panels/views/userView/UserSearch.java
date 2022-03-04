fdfa4ab0736383690b81fe569994c277786280bf
pembertonc <cedric.pemberton@gmail.com>
2022-03-04 08:18:10 -0400

    added method skeletons for creating DataTable

--- src/main/java/com/techmine/gs/ui/panels/views/userView/UserSearch.java
+++ src/main/java/com/techmine/gs/ui/panels/views/userView/UserSearch.java
@@ -15,12 +15,18 @@
  */
 package com.techmine.gs.ui.panels.views.userView;
 
+import com.techmine.gs.domain.Subject;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.data.IDataProvider;
 import org.apache.wicket.model.IModel;
 
 /**
  *
  * @author Cedric-Pemberton
+ *
+ * Displays a search results grid based on Wicket-extensions "Datatable" and a
+ * test field.
  */
 public class UserSearch extends Panel {
 
@@ -30,6 +36,20 @@ public class UserSearch extends Panel {
 
     public UserSearch(String id, IModel<?> model) {
         super(id, model);
+
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    public DataTable<Subject, String> initializeDataTable() {
+        throw new UnsupportedOperationException("Initialize table not yet implmented");
+    }
+
+    private IDataProvider getSubjectDataProvider() {
+        throw new UnsupportedOperationException("getSubjectDataProvider not yet implmented");
     }
 
 }
