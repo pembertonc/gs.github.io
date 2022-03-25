69d40c86f6a60dc29d254e634a21a84cfba6d3ed
pembertonc <cedric.pemberton@gmail.com>
2022-03-25 17:24:04 -0400

    Added Component to display and bind the the html select tag to display the gasType.   created  mock db for the gasType.

--- src/main/java/com/techmine/gs/domain/GasType.java
+++ src/main/java/com/techmine/gs/domain/GasType.java
@@ -1,5 +1,6 @@
 package com.techmine.gs.domain;
 
+import java.util.Objects;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -22,6 +23,14 @@ public class GasType extends BaseEntity {
     @NotBlank(message = "Gas Symbol is required")
     @Size(min = 1, max = 3, message = "Gas Symbol can not be greater than 3 characters")
     private String symbol;
+
+    public GasType(String name, String symbol) {
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    public GasType() {
+    }
 
     public String getName() {
         return name;
@@ -47,6 +56,41 @@ public class GasType extends BaseEntity {
     public GasType symbol(String symbol) {
         this.symbol = symbol;
         return this;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!Objects.equals(getClass(), obj.getClass())) {
+            return false;
+        }
+        final GasType other = (GasType) obj;
+        if (!java.util.Objects.equals(this.getId(), other.getId())) {
+            return false;
+        }
+        if (!java.util.Objects.equals(this.getName(), other.getName())) {
+            return false;
+        }
+        if (!java.util.Objects.equals(this.getSymbol(), other.getSymbol())) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + Objects.hashCode(this.getId());
+        hash = 31 * hash + Objects.hashCode(this.getName());
+        hash = 31 * hash + Objects.hashCode(this.getSymbol());
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "GasType{" + " name=" + name + ", symbol=" + symbol + '}';
     }
 
 }
